117b511dc7d76c16ec26f751544a15daf20fbead
Rokibul Hasan <rokibul@tasawr.com>
2014-09-01 17:22:40 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vrp::Application.config.secret_key_base = 'f3a64306569ef6d33ecc163faa25ae9c56b2e0bb42ca2b2d5016a87e759c9f016303406343874c6638c0ec80af3435331ec108235755e963cc4c3f44c8db814b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vrp::Application.config.session_store :cookie_store, key: '_vrp_session'
